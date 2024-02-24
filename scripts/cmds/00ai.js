b0076e0338a0a162c15ac80088af3caa0380c788
tchindaazice <155647915+tchindaazice@users.noreply.github.com>
2024-02-24 23:49:40 +0100

    Add files via upload

--- scripts/cmds/00ai.js
+++ scripts/cmds/00ai.js
@@ -1,69 +1,50 @@
 const axios = require('axios');
 
 const Prefixes = [
-  'gpt',
+  '/ai',
+  'bot',
+  '-tk',
+  '+ai',
+  'tk',
   'ai',
   'ask',
-  'chat',
-  'tk',
-  'bot',
 ];
 
 module.exports = {
   config: {
-    name: 'ai',
-    version: '2.5',
-    author: 'Elohime ', // do not change
-    role: 0,
-    category: 'ai2',
-    shortDescription: {
-      en: 'Asks an AI for an answer.',
-    },
-    longDescription: {
-      en: 'Asks an AI for an answer based on the user prompt.',
-    },
+    name: "ask",
+    version: 1.0,
+    author: "OtinXSandip",
+    longDescription: "AI",
+    category: "ai",
     guide: {
-      en: '{pn} [prompt]',
+      en: "{p} questions",
     },
   },
   onStart: async function () {},
   onChat: async function ({ api, event, args, message }) {
     try {
+      
       const prefix = Prefixes.find((p) => event.body && event.body.toLowerCase().startsWith(p));
-
       if (!prefix) {
-        return; 
+        return; // Invalid prefix, ignore the command
       }
-
       const prompt = event.body.substring(prefix.length).trim();
-
-      if (prompt === '') {
-        await message.reply(
-          "Veuillez poser la question à votre convenance et je m'efforcerai de vous fournir une réponse efficace🤓. Votre satisfaction est ma priorité absolue😼. (𝙀́𝙙𝙞𝙩 𝙗𝙮 𝙏𝙠 𝙅𝙤𝙚𝙡 ㋡)"
-        );
+   if (!prompt) {
+        await message.reply("Veuillez poser la question à votre convenance et je m'efforcerai de vous fournir une réponse efficace🤓. Votre satisfaction est ma priorité absolue😼. (𝙀́𝙙𝙞𝙩 𝙗𝙮 𝙏𝙠 𝙅𝙤𝙚𝙡 ㋡");
         return;
       }
 
+await message.reply("💬🧘🏾‍♂|veillez Patientez s'il-vous-plait...(𝙀́𝙙𝙞𝙩 𝙗𝙮 𝙏𝙠 𝙅𝙤𝙚𝙡 ㋡)");
 
-      await message.reply("💬🧘🏾‍♂️|veillez Patientez s'il-vous-plait...(𝙀́𝙙𝙞𝙩 𝙗𝙮 𝙏𝙠 𝙅𝙤𝙚𝙡 ㋡)");
+      const response = await axios.get(`https://sandipapi.onrender.com/gpt?prompt=${encodeURIComponent(prompt)}`);
+      const answer = response.data.answer;
 
-      const response = await axios.get(`https://chatgayfeyti.archashura.repl.co?gpt=${encodeURIComponent(prompt)}`);
+ 
+    await message.reply(answer);
 
-      if (response.status !== 200 || !response.data) {
-        throw new Error('Invalid or missing response from API');
-      }
-
-      const messageText = response.data.content.trim();
-
-      await message.reply(messageText);
-
-      console.log('Sent answer as a reply to user');
     } catch (error) {
-      console.error(`Failed to get answer: ${error.message}`);
-      api.sendMessage(
-        `${error.message}.\n\nYou can try typing your question again or resending it, as there might be a bug from the server that's causing the problem. It might resolve the issue.`,
-        event.threadID
-      );
+      console.error("Error:", error.message);
     }
-  },
+  }
 };
